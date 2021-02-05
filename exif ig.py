4a73657f4c47d7c2502661efa56456c65a9afd4f
IgyDigy <github@wormsbecher.com>
2021-02-05 20:50:11 +0100

    some data

--- exif ig.py
+++ exif ig.py
@@ -1,6 +1,6 @@
 from exif import Image
 
-with open("V:\\Foto-Film\\Jasper\\j.jpg", "rb") as image_file:
+with open(".\\test.jpg", "rb") as image_file:
     img = Image(image_file)
 print(dir(img))
 
--- getExif.py
+++ getExif.py
@@ -1,3 +1,6 @@
-bye
-
-ye 
+import PIL.ExifTags
+exif = {
+    PIL.ExifTags.TAGS[k]: v
+    for k, v in img._getexif().items()
+    if k in PIL.ExifTags.TAGS
+}
